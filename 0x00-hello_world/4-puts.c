9321545ef32bddafab881b9242b329debef3b402
Abigail <yeboah.abigail885@gmail.com>
2023-04-20 05:52:02 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
-/*
- * main - print a line of code using puts
+/**
+ * main - Entry point
  *
- * Retuns: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
